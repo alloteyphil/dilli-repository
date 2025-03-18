abf52ff7bbf6f525cddb0f51d049f509348e39bf
Philip Allotey <77976603+alloteyphil@users.noreply.github.com>
2025-03-18 22:10:18 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 const page = () => {
-  return <div></div>;
+  return (
+    <div className="bg-secondary px-2 py-10">
+      <div className="mx-auto max-w-[2000px]">
+        <h1 className="text-primary text-[200px] leading-[0.9] tracking-tight">
+          A Food Tale of Two Cities - Old Delhi & New Delhi, india.
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default page;
