24fe45fcdd46540e275e00ebd31825b1ce7e6dc6
Philip Allotey <77976603+alloteyphil@users.noreply.github.com>
2025-03-26 01:11:57 +0000

    updated nav-links

--- app/components/navbar/nav-links.tsx
+++ app/components/navbar/nav-links.tsx
@@ -1,28 +1,17 @@
 "use client";
 
 import Link from "next/link";
-import { useState, useRef } from "react";
-import { motion } from "motion/react";
 
 const NavLinks = () => {
-  const [position, setPosition] = useState({
-    left: 0,
-    width: 0,
-  });
-
-  const linksRef = useRef<(HTMLAnchorElement | null)[]>([]);
-
   const links = [
     {
       name: "Home",
       path: "/",
     },
-
     {
       name: "Menu",
       path: "/menu",
     },
-
     {
       name: "Private Dining",
       path: "/private-dining",
@@ -30,40 +19,13 @@ const NavLinks = () => {
   ];
 
   return (
-    <div
-      onMouseLeave={() => {
-        setPosition((prev) => ({
-          ...prev,
-          width: 0,
-        }));
-      }}
-      className="relative flex items-center gap-10"
-    >
-      {links.map((link, index) => (
-        <Link
-          key={link.path}
-          href={link.path}
-          ref={(el) => {
-            linksRef.current[index] = el;
-          }}
-          onMouseEnter={() => {
-            if (!linksRef.current[index]) return;
-
-            const { width } = linksRef.current[index].getBoundingClientRect();
-
-            setPosition({
-              left: linksRef.current[index].offsetLeft,
-              width: width,
-            });
-          }}
-        >
+    <div className="flex items-center gap-10">
+      {links.map((link) => (
+        <Link key={link.path} href={link.path} className="group relative">
           <p>{link.name}</p>
+          <div className="link-underline" />
         </Link>
       ))}
-      <motion.div
-        animate={position}
-        className="bg-accent absolute bottom-[2px] h-0.5"
-      ></motion.div>
     </div>
   );
 };
