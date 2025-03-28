2bc28225f39fc2676ae2bd2d82a01afa9884ee12
Philip Allotey <77976603+alloteyphil@users.noreply.github.com>
2025-03-28 01:07:32 +0000

    Update hero-private-dining.tsx

--- app/components/private-dining/hero-private-dining.tsx
+++ app/components/private-dining/hero-private-dining.tsx
@@ -1,13 +1,10 @@
-import Image from "next/image";
-import ellipse from "@/public/images/ellipse.svg";
-import Ellipse from "../shared/ellipse";
 import { CircleIcon } from "lucide-react";
 
 const HeroPrivateDining = () => {
   return (
-    <div className="bg-secondary py-3">
+    <div className="bg-secondary py-1">
       <div className="bg-accent relative h-[800px] w-screen max-w-[4000px]">
-        <div className="absolute -top-3 left-1/2 z-10 flex -translate-x-1/2 gap-[5px] overflow-x-hidden">
+        <div className="absolute -top-3 left-1/2 z-10 flex -translate-x-1/2 gap-[3px] overflow-x-hidden">
           {Array.from({ length: 400 }).map((_, index) => (
             <CircleIcon
               key={index}
@@ -15,7 +12,7 @@ const HeroPrivateDining = () => {
             />
           ))}
         </div>
-        <div className="absolute -bottom-3 left-1/2 z-10 flex -translate-x-1/2 gap-[5px] overflow-x-hidden">
+        <div className="absolute -bottom-3 left-1/2 z-10 flex -translate-x-1/2 gap-[3px] overflow-x-hidden">
           {Array.from({ length: 400 }).map((_, index) => (
             <CircleIcon
               key={index}
