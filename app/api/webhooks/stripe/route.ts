fe57b83ed60aaf3204c23c32c09cf67ddfa06cf1
Philip Allotey <77976603+alloteyphil@users.noreply.github.com>
2025-04-01 21:51:54 +0000

    Update route.ts

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server";
 import { createOrder } from "@/actions/order/order.actions";
 
 export async function POST(request: Request) {
-  console.log("Stripe webhook received");
   const body = await request.text();
 
   const sig = request.headers.get("stripe-signature") as string;
@@ -22,62 +21,30 @@ export async function POST(request: Request) {
 
   // CREATE
   if (eventType === "checkout.session.completed") {
-    // const { id, amount_total, metadata } = event.data.object;
+    const { id, amount_total, metadata } = event.data.object;
 
-    // const order = {
-    //   stripeId: id,
-    //   userDetails: {
-    //     email: metadata?.customer_email || "",
-    //     name: metadata?.customer_name || "",
-    //     address: {
-    //       city: JSON.parse(metadata?.shipping_address || "{}").city || "",
-    //       address: JSON.parse(metadata?.shipping_address || "{}").address || "",
-    //       address2:
-    //         JSON.parse(metadata?.shipping_address || "{}").address2 || "",
-    //       postCode:
-    //         JSON.parse(metadata?.shipping_address || "{}").post_code || "",
-    //     },
-    //   },
-    //   items: JSON.parse(metadata?.items || "[]"),
-    //   totalAmount: amount_total ? (amount_total / 100).toString() : "0",
-    //   createdAt: new Date(),
-    // };
-
-    // const newOrder = await createOrder(order);
-
-    const testOrderData = {
-      stripeId: "cs_test_123456789_112121",
+    const order = {
+      stripeId: id,
       userDetails: {
-        email: "test@example.com",
-        name: "John Doe",
+        email: metadata?.customer_email || "",
+        name: metadata?.customer_name || "",
         address: {
-          city: "New York",
-          address: "123 Main St",
-          address2: "Apt 4B",
-          postCode: "10001",
+          city: JSON.parse(metadata?.shipping_address || "{}").city || "",
+          address: JSON.parse(metadata?.shipping_address || "{}").address || "",
+          address2:
+            JSON.parse(metadata?.shipping_address || "{}").address2 || "",
+          postCode:
+            JSON.parse(metadata?.shipping_address || "{}").post_code || "",
         },
       },
-      items: [
-        {
-          id: "prod_123",
-          variant: "Small",
-          quantity: 2,
-        },
-        {
-          id: "prod_456",
-          variant: "Medium",
-          quantity: 1,
-        },
-      ],
-      totalAmount: "99.99",
+      items: JSON.parse(metadata?.items || "[]"),
+      totalAmount: amount_total ? (amount_total / 100).toString() : "0",
       createdAt: new Date(),
     };
 
-    const order = await createOrder(testOrderData);
+    const newOrder = await createOrder(order);
 
-    console.log(order);
-
-    return NextResponse.json({ message: "OK", order });
+    return NextResponse.json({ message: "OK", order: newOrder });
   }
 
   return new Response("", { status: 200 });
