503d43ae7a38997b6800715196f7098bdb721352
Philip Allotey <77976603+alloteyphil@users.noreply.github.com>
2025-04-04 23:25:20 +0000

    Update hero-private-dining.tsx

--- app/components/private-dining/hero-private-dining.tsx
+++ app/components/private-dining/hero-private-dining.tsx
@@ -5,24 +5,24 @@ import logo from "@/public/images/footerlogo.svg";
 
 const HeroPrivateDining = () => {
   return (
-    <section className="bg-secondary overflow-x-hidden pt-6 md:py-1">
+    <section className="bg-secondary">
       <div className="bg-accent relative grid w-screen max-w-screen place-items-center px-4 py-8 max-md:h-[80vh] md:max-w-[4000px] md:px-8 md:py-20 xl:px-0">
-        <div className="absolute -top-3 left-1/2 z-10 flex -translate-x-1/2 gap-[3px] overflow-x-hidden">
+        {/* <div className="absolute -top-3 left-1/2 z-10 flex max-w-screen -translate-x-1/2 gap-[3px] overflow-x-hidden md:max-w-[4000px]">
           {Array.from({ length: 400 }).map((_, index) => (
             <CircleIcon
               key={index}
               className="fill-secondary text-secondary size-[28px]"
             />
           ))}
-        </div>
-        <div className="absolute -bottom-3 left-1/2 z-10 flex -translate-x-1/2 gap-[3px] overflow-x-hidden">
+        </div> */}
+        {/* <div className="absolute -bottom-3 left-1/2 z-10 flex -translate-x-1/2 gap-[3px] overflow-x-hidden">
           {Array.from({ length: 400 }).map((_, index) => (
             <CircleIcon
               key={index}
               className="fill-secondary text-secondary size-[28px]"
             />
           ))}
-        </div>
+        </div> */}
         <div className="relative h-full w-full max-w-[1500px] md:h-[620px]">
           <div className="absolute inset-0 z-[5] bg-black opacity-40" />
           <Image
